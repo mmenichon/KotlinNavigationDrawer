815aa7000dbd38797076fca88cdf6d9442b3af7e
ManonM <105633434+mmenichon@users.noreply.github.com>
2023-01-13 11:53:16 +0100

    Implémentation Navigation View erreur au lancement de l'application

--- app/src/main/java/com/mmeni/kotlinfirstapplication/Controllers/Activities/MainActivity.kt
+++ app/src/main/java/com/mmeni/kotlinfirstapplication/Controllers/Activities/MainActivity.kt
@@ -5,40 +5,66 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.mmeni.kotlinfirstapplication.R
 
-class MainActivity(var toolbar: Toolbar, var drawnerLayout: DrawerLayout, var navigationView: NavigationView) : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity: AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    private var toolbar: Toolbar = findViewById(R.id.activity_main_toolbar)
+    private var drawnerLayout: DrawerLayout = findViewById(R.id.activity_main_drawer_layout)
+    private var navigationView: NavigationView = findViewById(R.id.activity_main_nav_view)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.configureToolBar()
+        this.configureDrawerLayout()
+        this.configureNavigationView()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (this.drawnerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawnerLayout.closeDrawer(GravityCompat.START)
+        }
+//        else {
+//            super.onBackPressed()
+//        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("Not yet implemented")
+
+        when (item.itemId) {
+            R.id.activity_main_drawer_news -> print("fragment news")
+            R.id.activity_main_drawer_profile -> print("profile")
+            R.id.activity_main_drawer_settings -> print("params")
+        }
+
+        this.drawnerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
     // ---------------------
     // CONFIGURATION
     // ---------------------
 
-    fun configureToolBar() {
-        this.toolbar = findViewById(R.id.activity_main_toolbar)
+    private fun configureToolBar() {
+//        this.toolbar = findViewById(R.id.activity_main_toolbar)
         setSupportActionBar(toolbar)
     }
 
-    fun configureDrawerLayout() {
-        this.drawnerLayout = findViewById(R.id.activity_main_drawer_layout)
+    private fun configureDrawerLayout() {
+//        this.drawnerLayout = findViewById(R.id.activity_main_drawer_layout)
         val toogle = ActionBarDrawerToggle(this, drawnerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawnerLayout.addDrawerListener(toogle)
         toogle.syncState()
     }
 
-    fun configureNavigationView() {
-        this.navigationView = findViewById(R.id.activity_main_nav_view)
+    private fun configureNavigationView() {
+//        this.navigationView = findViewById(R.id.activity_main_nav_view)
         navigationView.setNavigationItemSelectedListener(this)
     }
 
